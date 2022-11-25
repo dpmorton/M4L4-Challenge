9bf6bd01836f60983ca203fdd77c91ad04bdf948
David Morton <dpmorton@gmail.com>
2022-11-25 14:10:46 -0500

    Initial Commit

---
+++ M4L4 Challenge/M4L4_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  M4L4_ChallengeApp.swift
+//  M4L4 Challenge
+//
+//  Created by dpm on 11/25/22.
+//
+
+import SwiftUI
+
+@main
+struct M4L4_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
